0e696e5a3fb0e06db07b7a0160fa0b7e6ee0c9a6
Lorenzo Gaifas <brisvag@gmail.com>
2025-11-13 15:48:40 +0100

    fix other logos

--- tools/generate_logo_pngs.sh
+++ tools/generate_logo_pngs.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-
 this_dir=`dirname $(realpath "$0")`
 logos_dir="$this_dir/../logo/generated"
 
 for logo in $logos_dir/*svg;
 do
     png="${logo%.svg}.png"
-    inkscape $logo -o $png
+    inkscape $logo -o $png -w 1024 -h 1024
     echo `basename $png`
 done

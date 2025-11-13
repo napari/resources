f5e73dcddee1c0f1eab2e557b6de52c13bee01e1
Lorenzo Gaifas <brisvag@gmail.com>
2025-11-13 16:24:49 +0100

    roll back bold

--- tools/generate_font.py
+++ tools/generate_font.py
@@ -17,23 +17,23 @@ Inconsolata = TTFont(fonts / 'Inconsolata' / 'Inconsolata-Regular.ttf')
 missing_glyphs = set(Inconsolata.getGlyphOrder()) - set(Alata.getGlyphOrder())
 
 # for clarity
-napari_font = Alata
+InconsAlata = Alata
 
 for glyph in missing_glyphs:
-    napari_font['glyf'][glyph] = Inconsolata['glyf'][glyph]
-    napari_font['hmtx'][glyph] = Inconsolata['hmtx'][glyph]
+    InconsAlata['glyf'][glyph] = Inconsolata['glyf'][glyph]
+    InconsAlata['hmtx'][glyph] = Inconsolata['hmtx'][glyph]
 
 for table in Inconsolata['cmap'].tables:
     for codepoint, glyph_name in table.cmap.items():
-        if codepoint not in [cp for t in napari_font['cmap'].tables for cp in t.cmap]:
-            napari_font['cmap'].tables[0].cmap[codepoint] = glyph_name
+        if codepoint not in [cp for t in InconsAlata['cmap'].tables for cp in t.cmap]:
+            InconsAlata['cmap'].tables[0].cmap[codepoint] = glyph_name
 
-family_name = "napari"
+family_name = "InconsAlata"
 style_name = "Regular"
 full_name = f"{family_name} {style_name}"
-postscript_name = "napari-Regular"
+postscript_name = f"{family_name}-{style_name}"
 
-name_table = napari_font["name"]
+name_table = InconsAlata["name"]
 for record in name_table.names:
     if record.nameID == 1:
         record.string = family_name.encode(record.getEncoding())
@@ -44,4 +44,4 @@ for record in name_table.names:
     elif record.nameID == 6:
         record.string = postscript_name.encode(record.getEncoding())
 
-napari_font.save(fonts / 'napari' / f'{postscript_name}.ttf')
+InconsAlata.save(fonts / 'InconsAlata' / f'{postscript_name}.ttf')
